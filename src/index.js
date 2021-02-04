186f3d9ecfe27240b37d7a9217c01eb7f2760601
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-02-04 19:46:40 +0200

    add html and scss header, main

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './scss/layout/_mixin.scss';
+import './scss/layout/base.scss';
+import './scss/components/header.scss';
+import './scss/components/hero.scss';
+import './scss/components/visuality.scss';
+import './scss/components/confidence.scss';
+import './scss/components/gallery.scss';
+import './scss/components/how-work.scss';

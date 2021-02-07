4068532dba3469d1150dd0d81186e58360eba578
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-02-07 23:10:38 +0200

    home pages

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import './scss/components/visuality.scss';
 import './scss/components/confidence.scss';
 import './scss/components/gallery.scss';
 import './scss/components/how-work.scss';
+import './scss/components/who_i_am.scss';
+import './scss/components/bonus.scss';
+import './scss/components/contact_us.scss';
+import './scss/components/footer.scss';
